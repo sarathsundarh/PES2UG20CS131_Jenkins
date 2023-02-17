1838aad19483bb93aa10775a0768d6d6403f97f9
sarathsundarh <78008382+sarathsundarh@users.noreply.github.com>
2023-02-18 02:05:40 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,3 +1,8 @@
-int main(void){
-  printf("Hi this is a sample file I created");
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hi this is Sarath";
+  cout << "I have successfully built and run\n";
+  return 0;
 }
